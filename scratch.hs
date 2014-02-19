df70c06ddf065f03701862bdcfcb07cd1c989639
Peter Klipfel <peter@klipfel.me>
2014-02-18 23:55:04 -0700

    partial chapter 3

--- scratch.hs
+++ scratch.hs
@@ -1,18 +1,10 @@
-doubleMe x = x + x
-doubleUs x y = doubleMe x + doubleMe y
+filterLower :: [Char] -> [Char]
+filterLower str = [x | x <- str, elem x ['a'..'z'] ]
 
-doubleSmallNumber x = (if x < 100 then doubleMe x else x)
+cubedAddition :: Int -> Int -> Int -> Int
+cubedAddition a b c = a*a*a + b*b*b + c*c*c
 
-fact = "I'm watching the" ++ " Olympics"
+comparison = 3 `compare` 3
 
-strCompare = "this" > "that"
-
-lower3 = ['3'..'a']
-charNums = ['0'..'9']
-
-batman = (take 30 (cycle "na"))++" batman"
-
-powersOf2 = [product (take x (repeat 2)) | x <- [0..10]]
-
-triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
-rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+numToDouble :: (Num n) => n -> Double
+numToDouble i = ((i) :: Double)
